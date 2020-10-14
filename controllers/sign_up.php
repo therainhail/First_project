af457184dac18bd4584bd55de2dad5f2e4878373
therainhail <reihgit@gmail.com>
2020-10-14 15:22:08 +0300

    c10

--- controllers/sign_up.php
+++ controllers/sign_up.php
@@ -6,7 +6,7 @@ if(isset($_POST['auth_email'])) $auth_email = $_POST['auth_email'];
 if(isset($_POST['auth_name'])) $auth_name = $_POST['auth_name'];
 if(isset($_POST['auth_pass'])) $auth_pass = $_POST['auth_pass'];
 
-if(!empty($auth_email) && !empty($auth_name) && !empty($auth_pass)) {
+if(!empty($_POST['auth_email']) && !empty($_POST['auth_name']) && !empty($$_POST['auth_pass'])) {
 	header('url = http://localhost/index.php');
 }
 
