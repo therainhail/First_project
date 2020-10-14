eec6b241d8564b8a8ec5ffe463caaf5bf3644ec1
therainhail <reihgit@gmail.com>
2020-10-14 14:57:10 +0300

    c4

--- controllers/sign_up.php
+++ controllers/sign_up.php
@@ -2,16 +2,19 @@
 $auth_email="not";
 $auth_name = "not";
 $auth_pass = "not";
-if(isset($_POST['auth_email'])) $auth_email = $POST['auth_email'];
-if(isset($_POST['auth_name'])) $auth_name = $POST['auth_name'];
-if(isset($_POST['auth_pass'])) $auth_pass = $POST['auth_pass'];
+if(isset($_POST['auth_email'])) $auth_email = $_POST['auth_email'];
+if(isset($_POST['auth_name'])) $auth_name = $_POST['auth_name'];
+if(isset($_POST['auth_pass'])) $auth_pass = $_POST['auth_pass'];
+if(isset($_POST['auth_email'] and $_POST['auth_name'] and $_POST['auth_pass'])
+function Redirect ($url, $permanent = false)
+{
+	header('Location: ' .$url, true, $permanent ? 301:302);
+	exit();
+}
+Redirect ('http://localhost/', false);
+)
 
 print_r($_POST['auth_email']);
 print_r($_POST['auth_name']);
 print_r($_POST['auth_pass']);
-$myfile= fopen("newfile.txt","w") or die("U");
-fwrite($myfile, $_POST['auth_email']);
-fwrite($myfile, $_POST['auth_name']);
-fwrite($myfile, $_POST['auth_pass']);
-fclose($myfile);
 ?>
