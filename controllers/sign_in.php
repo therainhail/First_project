5a55cde1d8f337735b445cd4c20581d1ac3655b6
therainhail <reihgit@gmail.com>
2020-10-14 14:38:08 +0300

    Change1

--- controllers/sign_in.php
+++ controllers/sign_in.php
@@ -1,10 +1,9 @@
 <?php
 $auth_name = "not";
 $auth_pass = "not";
-if(isset($_POST['auth_name'])) $auth_name = $POST['auth_email'];
-if(isset($_POST['auth_pass'])) $auth_pass = $POST['auth_email'];
+if(isset($_POST['auth_name'])) $auth_name = $POST['auth_name'];
+if(isset($_POST['auth_pass'])) $auth_pass = $POST['auth_pass'];
 
-print_r($auth_email);
-print_r($auth_name);
-print_r($auth_pass);
+print_r($_POST['$auth_name']);
+print_r($_POST['$auth_pass']);
 ?>
--- controllers/sign_up.php
+++ controllers/sign_up.php
@@ -6,15 +6,9 @@ if(isset($_POST['auth_email'])) $auth_email = $POST['auth_email'];
 if(isset($_POST['auth_name'])) $auth_name = $POST['auth_name'];
 if(isset($_POST['auth_pass'])) $auth_pass = $POST['auth_pass'];
 
-$filename = 'data.txt';
-$f_hdl = fopen($filename, 'w');
-fwrite($f_hdl, $auth_email);
-fwrite($f_hdl, $auth_name);
-fwrite($f_hdl, $auth_pass);
-fclose($f_hdl);
-
+file_put_contents('data.txt', $auth_email);
 
 print_r($_POST['auth_email']);
 print_r($_POST['auth_name']);
-print_r(Ваш пароль: $_POST['auth_pass']);
+print_r($_POST['auth_pass']);
 ?>
