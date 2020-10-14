66b07459c83e668c75b8b91c08913094ce17cd55
therainhail <reihgit@gmail.com>
2020-10-14 14:42:55 +0300

    c2

--- controllers/sign_in.php
+++ controllers/sign_in.php
@@ -4,6 +4,8 @@ $auth_pass = "not";
 if(isset($_POST['auth_name'])) $auth_name = $POST['auth_name'];
 if(isset($_POST['auth_pass'])) $auth_pass = $POST['auth_pass'];
 
-print_r($_POST['$auth_name']);
-print_r($_POST['$auth_pass']);
+print_r($_POST['auth_name']);
+print_r($_POST['auth_pass']);
 ?>
+
+
