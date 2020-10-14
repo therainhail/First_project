bac5eec2ea4e727718f543f5e17c8d900c8db178
therainhail <reihgit@gmail.com>
2020-10-14 14:29:17 +0300

    change form4

--- controllers/sign_up.php
+++ controllers/sign_up.php
@@ -3,8 +3,16 @@ $auth_email="not";
 $auth_name = "not";
 $auth_pass = "not";
 if(isset($_POST['auth_email'])) $auth_email = $POST['auth_email'];
-if(isset($_POST['auth_name'])) $auth_name = $POST['auth_email'];
-if(isset($_POST['auth_pass'])) $auth_pass = $POST['auth_email'];
+if(isset($_POST['auth_name'])) $auth_name = $POST['auth_name'];
+if(isset($_POST['auth_pass'])) $auth_pass = $POST['auth_pass'];
+
+filename = 'data.txt';
+$f_hdl = fopen($filename, 'w');
+fwrite($f_hdl, $auth_email);
+fwrite($f_hdl, $auth_name);
+fwrite($f_hdl, $auth_pass);
+fclose($f_hdl);
+
 
 print_r($_POST['auth_email']);
 print_r($_POST['auth_name']);
