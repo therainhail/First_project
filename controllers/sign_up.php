e3d762dff4cc67c35a7837513924728747426737
therainhail <reihgit@gmail.com>
2020-10-14 15:09:47 +0300

    c7

--- controllers/sign_up.php
+++ controllers/sign_up.php
@@ -5,11 +5,13 @@ $auth_pass = "not";
 if(isset($_POST['auth_email'])) $auth_email = $_POST['auth_email'];
 if(isset($_POST['auth_name'])) $auth_name = $_POST['auth_name'];
 if(isset($_POST['auth_pass'])) $auth_pass = $_POST['auth_pass'];
-if(isset($_POST['auth_email'] and $_POST['auth_name'] and $_POST['auth_pass'])
+
+if(isset($_POST['auth_email']) and ($_POST['auth_name']) and ($_POST['auth_pass']));
 header('Location: ../index.php');
-)
+);
 
 print_r($_POST['auth_email']);
 print_r($_POST['auth_name']);
 print_r($_POST['auth_pass']);
+
 ?>
