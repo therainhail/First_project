0be1abb0759366ae2d43f040962f0aeb94bd3ec2
therainhail <reihgit@gmail.com>
2020-10-14 15:07:15 +0300

    c6

--- controllers/sign_up.php
+++ controllers/sign_up.php
@@ -6,13 +6,7 @@ if(isset($_POST['auth_email'])) $auth_email = $_POST['auth_email'];
 if(isset($_POST['auth_name'])) $auth_name = $_POST['auth_name'];
 if(isset($_POST['auth_pass'])) $auth_pass = $_POST['auth_pass'];
 if(isset($_POST['auth_email'] and $_POST['auth_name'] and $_POST['auth_pass'])
-
-function Redirect ("../index.php", $permanent = false)
-{
-	header('Location: ../index.php', true, $permanent ? 301:302);
-	exit();
-}
-Redirect ('../index.php', false);
+header('Location: ../index.php');
 )
 
 print_r($_POST['auth_email']);
