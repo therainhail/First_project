dc7a761c68d0c84147c63d0509a1ec480d832e97
therainhail <reihgit@gmail.com>
2020-10-14 14:47:37 +0300

    c3

--- controllers/sign_up.php
+++ controllers/sign_up.php
@@ -6,9 +6,12 @@ if(isset($_POST['auth_email'])) $auth_email = $POST['auth_email'];
 if(isset($_POST['auth_name'])) $auth_name = $POST['auth_name'];
 if(isset($_POST['auth_pass'])) $auth_pass = $POST['auth_pass'];
 
-file_put_contents('data.txt', $auth_email);
-
 print_r($_POST['auth_email']);
 print_r($_POST['auth_name']);
 print_r($_POST['auth_pass']);
+$myfile= fopen("newfile.txt","w") or die("U");
+fwrite($myfile, $_POST['auth_email']);
+fwrite($myfile, $_POST['auth_name']);
+fwrite($myfile, $_POST['auth_pass']);
+fclose($myfile);
 ?>
